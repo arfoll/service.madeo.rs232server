7dc10c40c5df9a3fe1b45b500559f3cafe7c57c1
Brendan Le Foll <brendan@fridu.org>
2013-02-24 20:46:48 +0000

    seperate off dbus_control

--- default.py
+++ default.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2011, 2012 Brendan Le Foll <brendan@fridu.net>
+# Copyright (C) 2011, 2012, 2013 Brendan Le Foll <brendan@fridu.net>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,24 +13,17 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import gobject
-gobject.threads_init()
-from dbus import glib
-glib.init_threads()
-import dbus
 import time
 import xbmc
 import xbmcgui
 import xbmcaddon
 import sys
 
+from dbus_control import DbusControl
+
 Addon = xbmcaddon.Addon(id='service.madeo.rs232server')
 __language__ = Addon.getLocalizedString
 
-#Dbus paths
-RS232SERVER_BUS_NAME = 'uk.co.madeo.rs232server'
-AZURSERVICE_OBJ_PATH = '/uk/co/madeo/rs232server/azur'
-AZURSERVICE_IFACE = 'uk.co.madeo.rs232server.azur'
 #volume control variables
 music = False
 video = False
@@ -42,49 +35,31 @@ IDLE = 0
 DIDPLAY = False
 NOTIFYDONE = False
 
-class DbusControl:
-  def __init__(self):
-    pass
-
-  def connect(self):
-    self.bus = dbus.SystemBus()
-    self.amp = self.bus.get_object(RS232SERVER_BUS_NAME, AZURSERVICE_OBJ_PATH)
-    self.iface = dbus.Interface(self.amp, AZURSERVICE_IFACE)
-
-  def checkIface(self):
-    try:
-      iface.check()
-      return True
-    except:
-      return False
-
-  def getIface(self):
-    if self.checkIface():
-      return self.iface
-    else:
-      self.connect()
-      return self.iface
-
 class Caller:
   def __init__(self):
     #for notify mode 1
     self.NOTIFYDONE = False
     self.POWER = False
     self.control = DbusControl()
+    self.control.startAzur()
+    self.control.startLgtv()
 
-  def send_cmd(self, msg, repeat):
-    self.control.getIface().send_cmd(msg, repeat, False)
+  def send_azur_cmd(self, msg, repeat):
+    self.control.getAmpIface().send_cmd(msg, repeat, False)
+
+  def send_lgtv_cmd(self, msg, repeat):
+    self.control.getLgtvIface().send_cmd(msg, repeat, False)
 
   def poweron(self):
     try:
-      self.send_cmd('poweron', 1)
+      self.send_azur_cmd('poweron', 1)
       self.POWER = True
     except:
       self.connectionError()
 
   def poweroff(self):
     try:
-      self.send_cmd('poweroff', 1)
+      self.send_azur_cmd('poweroff', 1)
       self.POWER = False
     except:
       self.connectionError()
@@ -101,7 +76,6 @@ class Caller:
   def powerStatus(self):
     return self.POWER
 
-
 caller = Caller()
 #xbmc.executeJSONRPC("{\"jsonrpc\": \"2.0\", \"method\": \"Application.SetVolume\", \"params\": { \"volume\": 100 }, \"id\": 1}")
 
@@ -111,24 +85,25 @@ while (not xbmc.abortRequested):
     IDLE = 0
     DIDPLAY = True
     lastcheck = time.time()
-    #Application.Property.Name(volume)
-    #Application.Property.Name(muted)
   # check play type
   if (xbmc.Player().isPlayingAudio()):
     music = True
     if (caller.powerStatus() is False):
       caller.poweron()
     if (video is True):
-      caller.send_cmd('voldown', int(Addon.getSetting('voldiff')))
+      caller.send_azur_cmd('voldown', int(Addon.getSetting('voldiff')))
       video = False
   elif (xbmc.Player().isPlayingVideo()):
     video = True
     if (caller.powerStatus() is False):
       caller.poweron()
     if (music is True):
-      caller.send_cmd('volup', int(Addon.getSetting('voldiff')))
+      caller.send_azur_cmd('volup', int(Addon.getSetting('voldiff')))
+      caller.send_lgtv_cmd('poweron', 1)
+      if (Addon.getSetting('muteonvid')):
+        caller.send_lgtv_cmd('mute', 1)
       music = False
-  #poweroff if hasn't played/idle for 120secs
+  #poweroff if hasn't played/idle for defined timeout
   elif (xbmc.getGlobalIdleTime() > int(Addon.getSetting('timeout'))) and (caller.powerStatus() is True or FIRSTRUN is True):
     if (IDLE == 0) and (DIDPLAY is False):
       caller.poweroff()
