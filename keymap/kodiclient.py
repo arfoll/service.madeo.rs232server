038a969d05c9d1d7725034f22ae4e1063b24648e
Brendan Le Foll <brendan@fridu.org>
2017-11-30 23:20:30 +0100

    kodiclient.py: Add clevermute
    Why is the ACTION1 checking not working/sporadic?
    
    Signed-off-by: Brendan Le Foll <brendan@fridu.org>

--- keymap/kodiclient.py
+++ keymap/kodiclient.py
@@ -1,13 +1,38 @@
 #!/usr/bin/env python2
 
+import os
 import sys
 import subprocess
+import errno
 
+if len(sys.argv) < 3:
+    print "error: Not enough arguments"
+    print "       xbmcclient.py <RS232_SERVICE> <ACTION>"
+    print "   ex: xbmcclient.py azur mute"
+    sys.exit(errno.EINVAL)
+
+CLEVERMUTE_LOCK="/tmp/clevermute"
 MINICLIENT_BINARY="/usr/bin/miniclient"
+RS232_SERVICE=sys.argv[1]
+ACTION1=sys.argv[2]
+#MUTEVOID=['unmute', 'poweroff', 'voldown', 'volup']
 
-args = [MINICLIENT_BINARY]
-args = args + sys.argv[1:len(sys.argv)]
+#if ACTION1 in MUTEVOID:
+#    try:
+#        os.remove(CLEVERMUTE_LOCK)
+#    except Exception:
+#        pass
+
+if ACTION1 == "clevermute":
+    if os.path.exists(CLEVERMUTE_LOCK):
+        sys.argv[2]="unmute"
+        os.remove(CLEVERMUTE_LOCK)
+    else:
+        f=open(CLEVERMUTE_LOCK, "w+")
+        sys.argv[2]="mute"
+
+args = [MINICLIENT_BINARY] + [RS232_SERVICE] + sys.argv[2:len(sys.argv)]
 print args
 
 subprocess.call(args)
-
+sys.exit(0)
